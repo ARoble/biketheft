c929b7873556d994c6d6a94cf1f778dc41b74ce1
Abdulladif Roble <45190868+ARoble@users.noreply.github.com>
2020-04-22 21:36:38 +0400

    Add files via upload

---
+++ admin/user_list.php
@@ -0,0 +1,142 @@
+<?php
+ob_start();
+session_start();
+// Database connection
+include ("config.php");
+include("check_login.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+        <meta charset="utf-8" />
+        <title>Admin Panel</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Control Panel" name="description" />
+        <meta content="Anti Theft" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="#">
+
+        <!-- plugins -->
+        <link href="assets/libs/flatpickr/flatpickr.min.css" rel="stylesheet" type="text/css" />
+
+        <!-- App css -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body>
+        <!-- Begin page -->
+        <div id="wrapper">
+
+            <!-- Topbar Start -->
+ <?php include("topbar.php")?>
+            <!-- end Topbar -->
+
+            <!-- ========== Left Sidebar Start ========== -->
+            <?php include("left_sidebar.php")?>
+            <!-- Left Sidebar End -->
+
+            <!-- ============================================================== -->
+            <!-- Start Page Content here -->
+            <!-- ============================================================== -->
+
+            <div class="content-page">
+                <div class="content">
+                    <div class="container-fluid">
+                        <div class="row page-title align-items-center">
+                            <div class="col-sm-4 col-xl-6">
+                                <h4 class="mb-1 mt-0">User List</h4>
+                            </div>
+                        </div>
+
+
+               
+                
+                        <!-- products -->
+                        <div class="row">
+                            <div class="col-xl-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title mt-0 mb-0 header-title">List</h5>
+
+                                        <div class="table-responsive mt-4">
+                                            <table class="table table-hover table-nowrap mb-0">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col">#</th>
+														<th scope="col">Full Name</th>
+                                                        <th scope="col">Email</th>
+                                                        <th scope="col">Badge Number</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+												  <?php
+$sql = "SELECT * FROM tb_admin";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+?>
+                                                    <tr>
+                                                        <td>#<?php echo$row["id"]; ?></td>
+														<td><?php echo$row["full_name"]; ?></td>
+                                                        <td><?php echo$row["email"]; ?></td>
+															<td><?php echo$row["badge_no"]; ?></td>
+                                                    </tr>
+<?php }} else{ ?>
+<td COLSPAN=4><center>None Found</center></td>
+<?php } ?>
+                                                </tbody>
+                                            </table>
+                                        </div> <!-- end table-responsive-->
+                                    </div> <!-- end card-body-->
+                                </div> <!-- end card-->
+                            </div> <!-- end col-->
+                        </div>
+                        <!-- end row -->
+
+                
+
+                <!-- Footer Start -->
+               <?php include("footer.php")?>
+                <!-- end Footer -->
+
+            </div>
+
+            <!-- ============================================================== -->
+            <!-- End Page content -->
+            <!-- ============================================================== -->
+
+
+        </div>
+        <!-- END wrapper -->
+
+      
+
+        <!-- Right bar overlay-->
+        <div class="rightbar-overlay"></div>
+
+        <!-- Vendor js -->
+        <script src="assets/js/vendor.min.js"></script>
+
+        <!-- optional plugins -->
+        <script src="assets/libs/moment/moment.min.js"></script>
+        <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+        <script src="assets/libs/flatpickr/flatpickr.min.js"></script>
+
+        <!-- page js -->
+        <script src="assets/js/pages/dashboard.init.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/app.min.js"></script>
+
+
+    </body>
+
+</html>
